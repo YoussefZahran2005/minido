9a84d49cfe8531474b095c87be85cd98e4c0f11e
YoussefZahran2005 <youssef.assem.zahran@gmail.com>
2026-09-08 11:59:36 +0300

    intial commit

---
+++ src/main/java/com/zahran/Task/Tracker/App/TaskTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.zahran.Task.Tracker.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zahran/Task/Tracker/App/TaskTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zahran.Task.Tracker.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
